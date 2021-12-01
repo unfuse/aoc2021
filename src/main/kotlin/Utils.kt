30936a8799268519d528fd65fe65b7f607dc6475
david.kennedy <david.kennedy@workday.com>
2021-12-01 13:54:55 -0800

    refactoring and utils

---
+++ src/main/kotlin/Utils.kt
@@ -0,0 +1,30 @@
+import java.io.File
+
+class Utils {
+
+    companion object {
+
+        // There's probably a snazzier way of doing this directly with collectors, but I can't quite get the types
+        //   to work out correctly, so I am relying on future me to get it.
+        fun <F, L> readFile(
+            fileName: String,
+            lineMapper: (line: String) -> L,
+            collectorSupplier: () -> F,
+            lineCollector: (item: L, collector: F) -> Unit
+        ): F {
+            val result = collectorSupplier.invoke()
+            File("/Users/david.kennedy/code/hacks/aoc2021/src/main/resources/$fileName.txt")
+                .readLines()
+                .map(lineMapper)
+                .stream()
+                .forEach {
+                    lineCollector.invoke(it, result)
+                }
+            return result
+        }
+
+        fun readFileAsIntList(fileName: String) : List<Int> {
+            return readFile(fileName, String::toInt, ::ArrayList) { item, collector -> collector += item }
+        }
+    }
+}
--- src/main/kotlin/day01.kts
+++ src/main/kotlin/day01.kts
@@ -1,13 +1,7 @@
-import java.io.File
-
-var nums: ArrayList<Int> = ArrayList()
-
-File("/Users/david.kennedy/code/hacks/aoc2021/src/main/resources/day01.txt").forEachLine {
-    nums.add(it.toInt())
-}
+val nums: List<Int> = Utils.readFileAsIntList("day01")
 
 // Part 1
-println(nums.windowed(2, 1).count { it[1] > it[0] })
+println(nums.windowed(2).count { it[1] > it[0] })
 
 // Part 2
-println(nums.windowed(3, 1) { it.sum() }.windowed(2, 1).count { it[1] > it[0] })
+println(nums.windowed(3) { it.sum() }.windowed(2).count { it[1] > it[0] })
